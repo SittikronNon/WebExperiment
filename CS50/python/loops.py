38f346e1dc374db56beaa9c9e2abb759918014d3
SittikronNon <sittikron.lamulsab@gmail.com>
2023-03-31 02:24:27 +0700

    python stuff

---
+++ CS50/python/loops.py
@@ -0,0 +1,4 @@
+wooh = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in wooh:
+    print(i)
---
+++ CS50/python/sequences.py
@@ -0,0 +1,8 @@
+names = ["Harry", "Ron", "Hermione"]
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
+
+print(names[0])
